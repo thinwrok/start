31d7d9491978077447bf435dc067dec0688d3dc6
JOSE CARLOS DE OLVEIIRA JUNIOR <junior@thinwork.com.br>
2026-05-01 06:35:32 -0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -36,20 +36,20 @@ echo "SELINUX=disabled" > /etc/selinux/config
 echo "PS1='\[\e[1;32m\]\h \[\e[1;31m\]@ \u \[\e[1;34m\]\w \[\e[1;31m\]\$ \[\e[0;37m\]'" >> ~/.bashrc
 echo "alias ls='ls --color=auto'" >> ~/.bashrc
 
-# Habiliar o root fazer login
-sudo passwd root
-sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-sudo systemctl restart ssh
-
 # Complementos
-# curl -sSL https://get.easypanel.io | sh
+#
+# ** easypanel
+# Install -- [ curl -sSL https://get.easypanel.io | sudo sh ]
+# Remove  -- [ sudo docker service rm easypanel traefik && sudo docker swarm leave --force && sudo docker system prune -a -f --volumes && sudo rm -rf /etc/easypanel ]
+#
 # curl -fsSL https://cdn.coollabs.io/coolify/install.sh | sudo bash
-# curl -sSL https://dokploy.com/install.sh | sh
+# curl -sSL https://dokploy.com/install.sh | sudo sh
 # curl -fsSL https://get.casaos.io/update | sudo bash
-# curl -sSL setup.oriondesign.art.br | sh
+# curl -sSL setup.oriondesign.art.br | sudo sh
 # URL=https://www.aapanel.com/script/install_panel_en.sh && if [ -f /usr/bin/curl ];then curl -ksSO $URL ;else wget --no-check-certificate -O install_panel_en.sh $URL;fi;bash install_panel_en.sh ipssl
 
-
+# Uninstall shelhost Feitos em docker 
+# sudo docker stop $(sudo docker ps -aq) && sudo docker rm -f $( sudo docker ps -aq)  && sudo docker container prune --force && sudo docker image prune --all --force && sudo docker volume prune --all --force && sudo docker system prune --all --volumes --force
 
 
 
