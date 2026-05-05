fd5ed5274cc107f333d06b49eef4003bb0ea1271
JOSE CARLOS DE OLVEIIRA JUNIOR <junior@thinwork.com.br>
2026-05-05 07:17:12 -0300

    Add ncdu and btop to package installation list

--- ubuntu.sh
+++ ubuntu.sh
@@ -17,11 +17,11 @@
 
 # Recursos
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y sudo curl wget nano htop mc tar rpm zip unzip git jq dialog openssl tmux fish rsync iputils-ping cpu-checker neofetch tilde lfm glances tcpdump wireshark 
+sudo apt install -y sudo curl wget nano htop mc tar rpm zip unzip git jq dialog openssl tmux fish rsync iputils-ping cpu-checker neofetch tilde lfm glances tcpdump wireshark ncdu btop
 sudo apt install -y apt-utils
 sudo apt install -y apache2-utils
-# sudo apt install -y python3
-# sudo apt install -y golang-go
+# sudo apt install -y python3 python3-pip
+# sudo apt install -y golang golang-go
 # sudo apt install -y nodejs
 
 
